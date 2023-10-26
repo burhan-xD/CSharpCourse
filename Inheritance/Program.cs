05c8c0d2181d60a828ae1ad654daaf8799979438
=burhan-xD <=burhanozsahinoglu@windowslive.com>
2023-10-26 19:00:23 +0300

    inheritance is added.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿// istanbulKodluyor FullStack .NET Eğitimi - Grup 1B
+// Conditionals
+// bunu yazan kişi öğrenme aşamasında ve kısıtlı ingilizce bilgisi olduğunda dolayı bazen türkçe, bazen ingilizce terimler, açıklamalar kullanmıştır.
+// Author: burhan-xD
+
+
+Customer customer = new Customer();
+Person[] person = new Person[3]
+{
+    new Customer{FirstName="Burhan"},
+    new Student{FirstName="xD"},
+    new Person{FirstName="istanbulKodluyor"},
+};
+
+foreach (Person p in person)
+{
+    Console.WriteLine(p.FirstName);
+}
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
+
+class Customer:Person
+{
+    public string City { get; set; }
+}
+
+class Student:Person
+{
+    public string University { get; set; }
+}
