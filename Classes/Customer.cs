42fa6d00d989f57d6e2980f8593e21e13a5aa93d
=burhan-xD <=burhanozsahinoglu@windowslive.com>
2023-10-26 15:41:50 +0300

    recapDemo-DAMA is added. (form app)

---
+++ Classes/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Customer
+    {
+
+        // get-set ile tanımlanmış değişkenler property'dir
+        // aşağıdaki gibi get- set olmadan düz değişken gibi tanımlanmış olanlara field denir.
+        // public string Name;
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -16,9 +16,9 @@ Console.WriteLine("*************************************");
 Console.WriteLine("");
 Console.ForegroundColor = ConsoleColor.White;
 
-
+                                                             // not 1a:
 CustomerManager customerManager = new CustomerManager();    // class'lar ile HEAP bölgesinde yeni bir tür oluşturmuş oluyoruz. onu kullanabilmek için 
-ProductManager productManager = new ProductManager();       // stack bölgesinden referans noktası oluşturmak gerekiyor. bunu bu şekilde yapıyoruz.
+ProductManager productManager = new ProductManager();      // stack bölgesinden referans noktası oluşturmak gerekiyor. bunu bu şekilde yapıyoruz.
 
 customerManager.Add();
 customerManager.Update();
@@ -28,7 +28,28 @@ Console.ForegroundColor = ConsoleColor.White;
 productManager.Add();
 productManager.Update();
 
-// Class'lar aynı birer dosya olarak tutulabilir.
+Customer customer = new Customer();
+customer.Id = 1;
+customer.FirstName = "Test";            // not 2a: atama işlemi yapıldığı zaman property'deki set işlemi çalışır
+customer.LastName = "Control";
+customer.City = "Zonguldak";
+
+Customer customer2 = new Customer
+{
+    Id = 2,
+    FirstName = "Control",
+    LastName = "Test",
+    City = "Antalya"
+};
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("*************************************");
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine(customer.City);       // not 2b: atanmış değer kullanıldığı zaman property içindeki get işlemi çalışır
+Console.WriteLine(customer2.City);
+
+// not 3a: Class'lar ayrı birer dosya olarak tutulabilir.
 
 //class CustomerManager
 //{
